0ad3b0ae4e3a24a2d572d348b2965d7fc15d1eb2
Cawley90 <chrisjam90@gmail.com>
2014-05-29 23:08:55 -0700

    forloop
    Gave the array values a data-value.  When a user clicks on an answer, it
    will fetch the value of the array object, put it through a for loop, and
    return the results through the console.
    
    If the answer is array [3] and the user clicks the 4th answer, it will
    only print out one answer.  Now I need to make a function that sees how
    many results it turns out.  If it only turns out one result, the user
    got the question right.

--- app.js
+++ app.js
@@ -2,25 +2,34 @@ $(document).ready(function(){
 	var qField = $('#qField');
 	var ansField = $('#ansField');
 	var nQuestion = [q1, q2];
-	resultScreen = $('#resultScreen');
+	var resultScreen = $('#resultScreen');
 
-
-
+ 	
 
 
 	
 function Problem(q, a){
 	this.q = q;
 	this.a = [];
-	this.a[0] = '<div id ="an0">' + a[0] + '</div>';
-	this.a[1] = '<div id ="an1">' + a[1] + '</div>';
-	this.a[2] = '<div id ="an2">' + a[2] + '</div>';
-	this.a[3] = '<div id ="an3">' + a[3] + '</div>';
-	
-	
+	this.a[0] = '<div data-value = 0 id ="ans">' + a[0] + '</div>';
+	this.a[1] = '<div data-value = 1 id ="ans">' + a[1] + '</div>';
+	this.a[2] = '<div data-value = 2 id ="ans">' + a[2] + '</div>';
+	this.a[3] = '<div data-value = 3 id ="ans">' + a[3] + '</div>';
 	
 
 }
+
+// x is the value of i, y is the question selector.
+function answerCheck(x, y){
+	this.x = x;
+	this.y = y;
+	//var arrVal = x.val();
+	for (var i = x; i<= y.length; i++) {
+		console.log(y[i]);
+	};
+}
+
+
 
 var q1 = new Problem("What do you do when you see someone post something inflammitory?",
 	["Argue with them until they realize the error in their ways, furthering your domination of the website and getting you closer to realizing your goal of being known as the website hero.",
@@ -29,8 +38,10 @@ var q1 = new Problem("What do you do when you see someone post something inflamm
 	"Don't click on it, don't post, just move on with your life.  This isn't the grand arena where good fights evil, it's just a website and you have better things to do."
 	]);
 
-var q2 = new Problem ("Color", ["Red", "Blue", "Green"]);
 
+
+
+var q2 = new Problem ("Color", ["Red", "Blue", "Green"]);
 
 
 
@@ -41,25 +52,19 @@ var q2 = new Problem ("Color", ["Red", "Blue", "Green"]);
 		qField.children().fadeOut(500);
 		ansField.children().fadeOut(500);
 		qField.append(q1.q);
-		//ansField.append('<div class="anStyle">' + q1.a + '</div>');
 		ansField.append(q1.a);
-		$(document).on("click", "#an0", function(){
-		resultScreen.fadeIn(500).append("<div id = 'text'>Incorrect! <br /> Sorry, but I think you should consider stepping away from your computer every-so-often </div>");
+		$(document).on("click", '#ans', function(){
+			//alert($(this).attr('data-value'));
+			answerCheck($(this).attr('data-value'), q1.a);
+		}); 
+			
+		
 
-		})
+		//$(document).on("click", "#an0", function(){
+		//resultScreen.fadeIn(500).append("<div id = 'text'>Incorrect! <br /> Sorry, but I think you should consider stepping away from your computer every-so-often </div>");
 
-		$(document).on("click", "#an1", function(){
-			resultScreen.fadeIn(500).append("<div id = 'text'>Incorrect! <br /> How noble. How wrong.</div>")
+		//})
 
-		})
-
-		$(document).on("click", "#an2", function(){
-			resultScreen.fadeIn(500).append("<div id = 'text'>Incorrect! <br /> I'll admit, sometimes funny things happen when we do this.  However, at some point you are going to start complaining about all the bad threads fully knowing you've been supporting them. Someone WILL call you out on that");
-		})
-
-		$(document).on("click", "#an3", function(){
-			resultScreen.fadeIn(500).append("<div id='text'>Correct! <br /> Who knew doing the right thing could be so easy?");
-		})
 		
 	});
 
