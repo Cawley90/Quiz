59ec23f1a8929522708b78a5f1187089543b3e70
Cawley90 <chrisjam90@gmail.com>
2014-05-29 17:17:03 -0700

    Build1.5
    Practicing with object constructors, seeing if I can get a question
    array going.  Also got it to append to the qField.

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ $(document).ready(function(){
 	var qField = $('#qField');
 	var ansField = $('#ansField');
 
+	// <--- q is question, a is answer, ca is correct answer --->
+	var question = {
+		q: [
+		"What's your favorite color?",
+		"Why is it your favorite color?",
+		"This array though",
+		],
+	};
+
+	console.log(question.q[2]);
+
+
+
 	// <--- When user clicks Start, it clears the welcome message and loads first question -->
 	$(document).on("click", "#startBtn", function(){
 		qField.children().fadeOut(500);
 		ansField.children().fadeOut(500);
+		qField.append(question.q[2]);
 	})
 
 });
