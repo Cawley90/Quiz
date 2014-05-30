32f15e27994eb14663b679360d5c9b7ab6fd9b13
Cawley90 <chrisjam90@gmail.com>
2014-05-29 20:06:16 -0700

    Onclick.answer
    When a user clicks on an answer, something happens.  Not happy with how
    I had to do it though.

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 $(document).ready(function(){
 	var qField = $('#qField');
 	var ansField = $('#ansField');
+	
 
 
 	// <--- q is question, c i, ca is correct answer --->
-function Dog(breed, shade){
-	this.breed = breed;
-	this.shade = shade;
+function Problem(q, a){
+	this.q = q;
+	this.a = [];
+	this.a[0] = '<div id ="an0">' + a[0] + '</div>';
+	this.a[1] = '<div id ="an1">' + a[1] + '</div>';
+	this.a[2] = '<div id ="an2">' + a[2] + '</div>';
+	this.a[3] = '<div id ="an3">' + a[3] + '</div>';
+	//this.a.setAttribute("class", "anStyle");
+	
+
 }
 
-var dog1 = new Dog("Corgi", "Tan");
+var q1 = new Problem("What's your favorite color?",
+	["Red", "Blue", "Green", "Yellow"]);
 
-console.log(dog1);
+
 	
 	// <--- When user clicks Start, it clears the welcome message and loads first question -->
 	$(document).on("click", "#startBtn", function(){
 		qField.children().fadeOut(500);
 		ansField.children().fadeOut(500);
-		qField.append(dog1.breed);
-		ansField.append(dog1.shade);
+		qField.append(q1.q);
+		//ansField.append('<div class="anStyle">' + q1.a + '</div>');
+		ansField.append(q1.a);
+
 		
 	})
 
+	$(document).on("click", "#an0", function(){
+		alert("It worked bitch");
+	})
+	
 });
