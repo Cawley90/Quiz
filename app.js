a706d50799f25294ef78473c01063e4a8b403a44
Cawley90 <chrisjam90@gmail.com>
2014-06-02 17:47:52 -0700

    build3
    Question check is done, current question is working, next question
    loader is working.  Is this real life?

--- app.js
+++ app.js
@@ -3,28 +3,35 @@ $(document).ready(function(){
 	var ansField = $('#ansField');
 	var nQuestion = [q1, q2];
 	var resultScreen = $('#resultScreen');
-	
-		
+	var currentQ = 0;
+
+
+
+function loadNext() {
+	var qArray = [q1, q2, q3, q4, q5];
+	currentQ = (currentQ+1)%qArray.length;
+	qField.children().fadeOut(500);
+	qField.empty();
+	ansField.children().fadeOut(500);
+	qField.append(qArray[currentQ].q);
+	ansField.append(qArray[currentQ].a);
+
+}
+
  //sel = slected answer, ca = correct answer, ln = loadnext, meaning which question comes after the one in question.  Doing this to cut down the code I have to do.
-function answerCheck (sel, ca, ln) {
+function answerCheck (sel, ca) {
 	this.sel = sel;
 	this.ca = ca;
 	if (sel == ca) {
 		alert("Yis");
-		loadNext(ln);
+		loadNext();
+		
 	}
 
 	else {
 		alert("Nah");
 	}
-}
 
-function loadNext(nextQ) {
-	this.nextQ = nextQ;
-	qField.children().fadeOut(500);
-	ansField.children().fadeOut(500);
-	qField.append(nextQ.q);
-	ansField.append(nextQ.a);
 }
 
 	
@@ -32,8 +39,8 @@ function Problem(q, a, correctAns){
 	this.q = q;
 	this.a = [];
 	this.correctAns = correctAns;
-	//data-value is assigning a number to run through the for loop so
-	//i can determine when to stop and return a successful number.
+	//data-value is assigning a number to run through the answer check so
+	//it can determine when to stop and return a successful number.
 	this.a[0] = '<div data-value = 0 id ="ans">' + a[0] + '</div>';
 	this.a[1] = '<div data-value = 1 id ="ans">' + a[1] + '</div>';
 	this.a[2] = '<div data-value = 2 id ="ans">' + a[2] + '</div>';
@@ -41,6 +48,7 @@ function Problem(q, a, correctAns){
 	
 
 }
+
 
 
 
@@ -53,8 +61,34 @@ var q1 = new Problem("What do you do when you see someone post something inflamm
 
 
 
-//dummy question for when I figure out how to switch to the next question.  Don't mind it.
-var q2 = new Problem ("Color", ["Red", "Blue", "Green"]);
+
+var q2 = new Problem("You've made a topic trying to influence discussion.  People start commenting.  What do you do?",
+	["Reply to it with one word.  Maybe even one letter would suffice.",
+	"Ignore all replies.",
+	"Change the name of the topic and make everyone look ridiculous.",
+	"Provide insightful comments, actually talk with the people in your thread.  Otherwise known as supporting your thread."
+	], 3);
+
+var q3 = new Problem("When is it okay to use rage faces?",
+	["In a VERY specific circumstance where it is extremely applicable.",
+	"In real life as a bumper sticker.",
+	"It's not okay, get it off my screen.",
+	"I don't know"
+	], 0);
+
+var q4 = new Problem("What's the difference between a meme and an image macro?",
+	["Nothing!  I took a picture of my friend making a face, I love internet maymays!",
+	"A meme is slightly akin to something going viral.  It's a particular image, video, or even text that is highly reguarded as funny and shared until it's almost common knowledge.  An image macro is text pasted onto an image with some sort of humor intended relating to the image.",
+	"I don't know and none of these answers seem to make sense.  Google is my favorite web browser, I use it to check my email at AOL.",
+	"RAGEFACE LOL"
+	], 1);
+
+var q5 = new Problem("What's a common trait between all websites?",
+	["They're all on the internet!",
+	"They all have a basis for their existence.  They're there to serve a purpose.",
+	"Love it or hate it, somewhere on this website, there's a picture or video of a cat. ",
+	"www...?"
+	], 2);
 
 
 
@@ -63,27 +97,47 @@ var q2 = new Problem ("Color", ["Red", "Blue", "Green"]);
 	
 	// <--- When user clicks Start, it clears the welcome message and loads first question -->
 	$(document).on("click", "#startBtn", function(){
+		//currentQ++
 		qField.children().fadeOut(500);
+		qField.empty();
 		ansField.children().fadeOut(500);
+		//loadNext();
 		qField.append(q1.q);
 		ansField.append(q1.a);
+		
 		$(document).on("click", '#ans', function(){
 			//alert($(this).attr('data-value'));
-			answerCheck($(this).attr('data-value'), q1.correctAns, q2);
+			if (currentQ == 0) {
+				console.log(currentQ);
+				answerCheck($(this).attr('data-value'), q1.correctAns);
+			}
+
+			else if (currentQ == 1) {
+				answerCheck($(this).attr('data-value'), q2.correctAns);
+			}
+
+			else if (currentQ == 2) {
+				answerCheck($(this).attr('data-value'), q3.correctAns);
+			}
+
+			else if (currentQ == 3) {
+				answerCheck($(this).attr('data-value'), q4.correctAns);
+			}
+			
+			else if (currentQ == 4) {
+				answerCheck($(this).attr('data-value'), q5.correctAns);
+			}
+
+			
+			
+
+
 									
 		}); 	
 		
 	});
 
-	$(document).on("click", "#nextQ", function(){
-		qField.children().fadeOut(500);
-		ansField.children().fadeOut(500);
-		$('#text').empty();
-		$('#resultScreen').fadeOut(100);
-
-	})
-	
-
 
 	
 });
+
