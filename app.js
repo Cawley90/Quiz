0a83e472607cf4a8e7307379e5fb00095c20e63b
Cawley90 <chrisjam90@gmail.com>
2014-06-04 00:11:20 -0700

    Finalv1
    Because nothing is ever completely final.  Just got rid of a useless
    function.

--- app.js
+++ app.js
@@ -163,13 +163,6 @@ var q12 = new Problem("Wikipedia..",
 	], 2);
 
 
-function fin() {
-		qField.children().fadeOut(300);
-		qField.empty();
-		ansField.children().fadeOut(300);
-		ansField.append("Dun");
-}
-
 	
 	// When user clicks Start, it clears the welcome message and loads first question.  
 	$(document).on("click", "#startBtn", function(){
